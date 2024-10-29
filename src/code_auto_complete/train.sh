67ce7092fd34b6f1906bb8b1d02c338cb6912933
Scc_hy <296294812@qq.com>
2024-10-29 16:46:43 +0800

    [ cac ]- ✨ Add: 服务器2GPU  config_large运行

--- src/code_auto_complete/train.sh
+++ src/code_auto_complete/train.sh
@@ -1,3 +1,6 @@
+source ~/.bashrc
+
 accelerate launch  cac_train_new.py
 # accelerate launch --mixed_precision="fp16"  --num_processes=2 cac_train_new.py
 # export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH
+
